a7b35c289aca4220f7d8cc12e32068536d6dc6d4
Riku Laukkanen <riku.laukkanen@nitor.com>
2021-10-04 14:16:48 +0300

    use client to write data to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import { client } from "./ApiClient";
+import { sql } from "./database";
+import { Data } from "./types";
+
+const writeMeasurementPointsIntoDb = (data: Data) => {
+  data.map(async (d) => {
+    await sql.raw(
+      `INSERT INTO Measurement (value, timestamp, measurement_point_name) VALUES (${d.value}, ${d.timeStamp}, ${d.measurementPoint.name});`
+    );
+  });
+};
+
+const start = () => {
+  setInterval(async () => {
+    client.startPolling();
+    await writeMeasurementPointsIntoDb(client.latest);
+  }, 10000);
+};
+
+start();
